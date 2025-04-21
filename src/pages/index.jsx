5b17a046ad5184664a2914d2a4f2a83d81cc507a
diogo-lima-flooxai <diogo.lima.flooxai@gmail.com>
2025-04-21 12:34:42 -0300

    feat: add API integration using async and await

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,6 +16,7 @@ export default function Inicio(){
       <Navegador texto= "Navegacao #01" destino='/navegacao/' cor= 'green'/>
       <Navegador texto= "Navegacao #02" destino='/cliente/sp-2/321' cor= 'blue'/>
       <Navegador texto= "Componente com Estado" destino='/estado' cor= 'pink'/>
+      <Navegador texto= "Integração com API #01" destino='/integracao_1' cor= 'yellow'/>
     </div>
   )
 }
